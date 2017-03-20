330d37a5919930358553423c8d2b49e26f4ad130
stevemk14ebr <stevemk14ebr@gmail.com>
2017-03-19 23:47:08 -0400

    Add capstone to cmake config

--- PolyHook.h
+++ PolyHook.h
@@ -1,11 +1,13 @@
 #ifndef POLYHOOK_2_0_POLYHOOK_H
 #define POLYHOOK_2_0_POLYHOOK_H
+
 #define DEBUG_MODE (0)
 #define ARCH_WIN (0)
 #define ARCH_LIN (1)
 #define MODE_x64 (0)
 #define MODE_x86 (1)
 
+#include "capstone/include/capstone/capstone.h"
 #include <string>
 #include <functional>
 #include <vector>
