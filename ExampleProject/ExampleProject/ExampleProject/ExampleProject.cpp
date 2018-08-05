23244637e31fc4a6d279efc63864fa9eb03711f1
Steve Eckels <stevemk14ebr@gmail.com>
2018-08-05 19:42:39 -0400

    Added ability to build as library and added example project

---
+++ ExampleProject/ExampleProject/ExampleProject/ExampleProject.cpp
@@ -0,0 +1,30 @@
+﻿// ExampleProject.cpp : Defines the entry point for the application.
+//
+
+#include "ExampleProject.h"
+#include "headers/CapstoneDisassembler.hpp"
+#include "headers/Detour/x86Detour.hpp"
+
+#include <cstdarg>
+uint64_t hookPrintfTramp = NULL;
+NOINLINE int __cdecl h_hookPrintf(const char* format, ...) {
+	char buffer[512];
+	va_list args;
+	va_start(args, format);
+	vsprintf_s(buffer, format, args);
+	va_end(args);
+
+	return PLH::FnCast(hookPrintfTramp, &printf)("INTERCEPTED YO:%s", buffer);
+}
+
+int main()
+{
+	PLH::CapstoneDisassembler dis(PLH::Mode::x86);
+	PLH::x86Detour detour((char*)&printf, (char*)&h_hookPrintf, &hookPrintfTramp, dis);
+	detour.hook();
+
+	printf("%s %f\n", "hi", .5f);
+	detour.unHook();
+	getchar();
+	return 0;
+}
