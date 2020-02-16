a3a8c3b69d7a0677cf9c97372a26ca93b1fd7a35
xeropresence <3128949+xeropresence@users.noreply.github.com>
2020-02-16 14:26:48 -0500

    Add the ability to set a log level to filter log messages

--- polyhook2/ErrorLog.hpp
+++ polyhook2/ErrorLog.hpp
@@ -15,23 +15,30 @@ struct Error {
 
 class ErrorLog {
 public:
+	void setLogLevel(ErrorLevel level) {
+		m_logLevel = level;
+	}
+	
 	void push(std::string msg, ErrorLevel level) {
-		push({ std::move(msg), level });
+		if (level >= m_logLevel)
+			push({ std::move(msg), level });
 	}
 
 	void push(Error err) {
-		switch (err.lvl) {
-		case ErrorLevel::INFO:
-			std::cout << "[+] Info: " << err.msg << std::endl;
-			break;
-		case ErrorLevel::WARN:
-			std::cout << "[!] Warn: " << err.msg << std::endl;
-			break;
-		case ErrorLevel::SEV:
-			std::cout << "[!] Error: " << err.msg << std::endl;
-			break;
-		default:
-			std::cout << "Unsupported error message logged " << err.msg << std::endl;
+		if (err.lvl >= m_logLevel) { 
+			switch (err.lvl) {
+			case ErrorLevel::INFO:
+				std::cout << "[+] Info: " << err.msg << std::endl;
+				break;
+			case ErrorLevel::WARN:
+				std::cout << "[!] Warn: " << err.msg << std::endl;
+				break;
+			case ErrorLevel::SEV:
+				std::cout << "[!] Error: " << err.msg << std::endl;
+				break;
+			default:
+				std::cout << "Unsupported error message logged " << err.msg << std::endl;
+			}
 		}
 		
 		m_log.push_back(std::move(err));
@@ -52,6 +59,7 @@ public:
 	}
 private:
 	std::vector<Error> m_log;
+	ErrorLevel m_logLevel = ErrorLevel::INFO;
 };
 
 
