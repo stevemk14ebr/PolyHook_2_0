e21fa4334c6dc863b995e0851366562e99659f9c
Luís Leite <luis@leite.xyz>
2020-03-16 23:17:33 +0000

    Fix '-Wdelete-non-abstract-non-virtual-dtor' warning

--- UnitTests/TestVFuncSwapHook.cpp
+++ UnitTests/TestVFuncSwapHook.cpp
@@ -9,6 +9,8 @@ EffectTracker vFuncSwapEffects;
 
 class VirtualTest2 {
 public:
+	virtual ~VirtualTest2() {}
+
 	virtual int NoParamVirt() {
 		return 4;
 	}
@@ -25,19 +27,19 @@ PLH::VFuncMap origVFuncs2;
 
 NOINLINE int __fastcall hkVirtNoParams2(uintptr_t pThis) {
 	vFuncSwapEffects.PeakEffect().trigger();
-	return ((tVirtNoParams)origVFuncs2.at(0))(pThis);
+	return ((tVirtNoParams)origVFuncs2.at(1))(pThis);
 }
 
 NOINLINE int __fastcall hkVirt2NoParams2(uintptr_t pThis) {
 	vFuncSwapEffects.PeakEffect().trigger();
-	return ((tVirtNoParams)origVFuncs2.at(1))(pThis);
+	return ((tVirtNoParams)origVFuncs2.at(2))(pThis);
 }
 
 TEST_CASE("VFuncSwap tests", "[VFuncSwap]") {
 	std::shared_ptr<VirtualTest2> ClassToHook(new VirtualTest2);
 
 	SECTION("Verify vfunc redirected") {
-		PLH::VFuncMap redirect = {{(uint16_t)0, (uint64_t)&hkVirtNoParams2}};
+		PLH::VFuncMap redirect = {{(uint16_t)1, (uint64_t)&hkVirtNoParams2}};
 		PLH::VFuncSwapHook hook((char*)ClassToHook.get(), redirect, &origVFuncs2);
 		REQUIRE(hook.hook());
 		REQUIRE(origVFuncs2.size() == 1);
@@ -49,7 +51,7 @@ TEST_CASE("VFuncSwap tests", "[VFuncSwap]") {
 	}
 
 	SECTION("Verify multiple vfunc redirected") {
-		PLH::VFuncMap redirect = {{(uint16_t)0, (uint64_t)&hkVirtNoParams2},{(uint16_t)1, (uint64_t)&hkVirt2NoParams2}};
+		PLH::VFuncMap redirect = {{(uint16_t)1, (uint64_t)&hkVirtNoParams2},{(uint16_t)2, (uint64_t)&hkVirt2NoParams2}};
 		PLH::VFuncSwapHook hook((char*)ClassToHook.get(), redirect, &origVFuncs2);
 		REQUIRE(hook.hook());
 		REQUIRE(origVFuncs2.size() == 2);
