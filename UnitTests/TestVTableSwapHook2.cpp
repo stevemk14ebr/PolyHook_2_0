ccfcaa7be236e3a87eb28d01c3b1456b15f95699
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-08-01 13:46:45 +0100

    VTableSwapHook2 template class for simpler API (#73)
    * New VTableSwapHook2 template class to simplify use of VTableSwapHook (draft).
    
    * Refactoring following reviewer comments.
    
    * Move make_vfunc_map to VTableSwapHook (as requested by review).
    * Move origFunc to VTableSwapHook.
    * No exceptions thrown (however, note use of std::map at() in origFunc, which may throw if origFunc is called with wrong template argument).
    * Update tests to use effects tracker.
    
    It would be possible to add a static_assert to ensure origFunc is not called with an invalid VFunc template parameter, if VTableSwapHook would take the VFuncTypes as a template argument. See comment in code.

---
+++ UnitTests/TestVTableSwapHook2.cpp
@@ -0,0 +1,83 @@
+#include <memory>
+#include <stdexcept>
+
+#include <Catch.hpp>
+
+#include "polyhook2/Virtuals/VTableSwapHook.hpp"
+#include "polyhook2/Tests/StackCanary.hpp"
+#include "polyhook2/Tests/TestEffectTracker.hpp"
+
+EffectTracker vTblSwapEffects2;
+
+// original class
+
+class MyClass {
+public:
+	virtual ~MyClass() {}
+
+	virtual int method1(int x) {
+		return 2 * x;
+	}
+
+	virtual int method2(int x, int y) {
+		return x + y;
+	}
+};
+
+// virtual function hooks
+
+int myclass_method1(MyClass* pThis, int x);
+int myclass_method2(MyClass* pThis, int x, int y);
+
+// helper typedefs, and unique_ptr for storing the hook
+
+typedef PLH::VFunc<1, decltype(&myclass_method1)> VMethod1;
+typedef PLH::VFunc<2, decltype(&myclass_method2)> VMethod2;
+std::unique_ptr<PLH::VTableSwapHook> hook = nullptr;
+
+// hook implementations
+
+NOINLINE int myclass_method1(MyClass* pThis, int x) {
+	vTblSwapEffects2.PeakEffect().trigger();
+	return hook->origFunc<VMethod1>(pThis, x) + 1;
+}
+
+NOINLINE int myclass_method2(MyClass* pThis, int x, int y) {
+	vTblSwapEffects2.PeakEffect().trigger();
+	return hook->origFunc<VMethod2>(pThis, x, y) + 2;
+}
+
+// test case
+
+TEST_CASE("VTableSwap2 tests", "[VTableSwap2]") {
+	auto ClassToHook = std::make_shared<MyClass>();
+
+	SECTION("Verify vtable redirected") {
+		PLH::StackCanary canary;
+		REQUIRE(ClassToHook->method1(3) == 6);
+		REQUIRE(ClassToHook->method2(13, 9) == 22);
+		hook = std::make_unique<PLH::VTableSwapHook>(
+			reinterpret_cast<uint64_t>(ClassToHook.get()),
+			VMethod1(&myclass_method1),
+			VMethod2(&myclass_method2));
+		REQUIRE(hook->hook());
+
+		vTblSwapEffects2.PushEffect();
+		REQUIRE(ClassToHook->method1(3) == 7);
+		REQUIRE(vTblSwapEffects2.PopEffect().didExecute());
+
+		vTblSwapEffects2.PushEffect();
+		REQUIRE(ClassToHook->method2(13, 9) == 24);
+		REQUIRE(vTblSwapEffects2.PopEffect().didExecute());
+
+		REQUIRE(hook->unHook());
+
+		vTblSwapEffects2.PushEffect();
+		REQUIRE(ClassToHook->method1(3) == 6);
+		REQUIRE(!vTblSwapEffects2.PopEffect().didExecute());
+
+		vTblSwapEffects2.PushEffect();
+		REQUIRE(ClassToHook->method2(13, 9) == 22);
+		REQUIRE(!vTblSwapEffects2.PopEffect().didExecute());
+	}
+}
--- polyhook2/Virtuals/VTableSwapHook.hpp
+++ polyhook2/Virtuals/VTableSwapHook.hpp
@@ -10,15 +10,47 @@
 #include "polyhook2/Misc.hpp"
 
 namespace PLH {
+
 typedef std::map<uint16_t, uint64_t> VFuncMap;
+
+// storage class for address of a virtual function
+// also stores the function pointer type and index number on the class level
+template<uint16_t I, typename FuncPtr>
+struct VFunc {
+	VFunc() : func(nullptr) {};
+	VFunc(FuncPtr f) : func(f) {};
+	const FuncPtr func;
+	static const uint16_t func_index;
+	typedef FuncPtr func_type;
+};
+
+// definition of constant must reside outside class declaration
+template<uint16_t I, typename FuncPtr> const uint16_t VFunc<I, FuncPtr>::func_index = I;
 
 class VTableSwapHook : public PLH::IHook {
 public:
+	VTableSwapHook(const uint64_t Class);
 	VTableSwapHook(const uint64_t Class, const VFuncMap& redirectMap);
 	VTableSwapHook(const char* Class, const VFuncMap& redirectMap);
+
+	template<uint16_t I, typename FuncPtr, typename ... VFuncTypes>
+	VTableSwapHook(const uint64_t Class, VFunc<I, FuncPtr> vfunc, VFuncTypes ... vfuncs)
+		: VTableSwapHook(Class, vfuncs ...)
+	{
+		m_redirectMap[I] = reinterpret_cast<uint64_t>(vfunc.func);
+	};
+
 	virtual ~VTableSwapHook() = default;
 
 	const VFuncMap& getOriginals() const;
+
+	template<typename VFuncType, typename ... Args>
+	auto origFunc(Args&& ... args) {
+		// NOTE: could do extra type check if VFuncTypes were a template argument of the class
+		// static_assert(std::disjunction_v<std::is_same<VFuncType, VFuncTypes> ...>);
+		auto func = reinterpret_cast<typename VFuncType::func_type>(m_origVFuncs.at(VFuncType::func_index));
+		return func(std::forward<Args>(args) ...);
+	};
 
 	virtual bool hook() override;
 	virtual bool unHook() override;
@@ -40,5 +72,7 @@ private:
 	VFuncMap m_origVFuncs;
 	bool m_Hooked;
 };
+
 }
+
 #endif
