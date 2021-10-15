ef3bd02bd91593be294c0cb0630b6e8b3bc856f2
denzo9 <denzo980@gmail.com>
2021-10-15 19:56:10 +0300

    Fix integer undeflow

--- sources/EatHook.cpp
+++ sources/EatHook.cpp
@@ -36,7 +36,7 @@ bool PLH::EatHook::hook() {
 		MemoryProtector protector(m_trampoline, 64, ProtFlag::R | ProtFlag::W | ProtFlag::X, *this, false);
 
 		PLH::ADisassembler::writeEncoding(makeAgnosticJmp(m_trampoline, m_fnCallback), *this);
-		offset = m_trampoline - m_moduleBase;
+		offset = static_cast<size_t>(m_trampoline - m_moduleBase);
 
 		Log::log("EAT hook offset is > 32bit's. Allocation of trampoline necessary", ErrorLevel::INFO);
 	}
