fc717e0fccc98e597bec743849b10c27c6e999fe
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-07-23 14:39:15 -0300

    Add PLH_INLINE macro for force inlining
    Adds alternative force inlining keywords for GCC and clang, and a fallback for other compilers

--- polyhook2/PolyHookOs.hpp
+++ polyhook2/PolyHookOs.hpp
@@ -2,46 +2,46 @@
 #define POLYHOOK_2_OS_HPP
 
 #if defined(WIN64) || defined(_WIN64) || defined(__MINGW64__)
-    #define POLYHOOK2_OS_WINDOWS
-    #define POLYHOOK2_ARCH_X64
+#define POLYHOOK2_OS_WINDOWS
+#define POLYHOOK2_ARCH_X64
 
-    #ifdef __GNUC__
-    
-    // VirtualAlloc2 requires NTDII_WIN10_RS4 on my distrubition of mingw
-    #define NTDDI_VERSION NTDDI_WIN10_RS4 
+#ifdef __GNUC__
 
-    // This was taken from Microsofts Detours library 
-    #define ERROR_DYNAMIC_CODE_BLOCKED 1655L
+// VirtualAlloc2 requires NTDII_WIN10_RS4 on my distrubition of mingw
+#define NTDDI_VERSION NTDDI_WIN10_RS4 
 
-    #endif
+// This was taken from Microsofts Detours library 
+#define ERROR_DYNAMIC_CODE_BLOCKED 1655L
+
+#endif
 
 #elif defined(WIN32) || defined(_WIN32) || defined(__MINGW32__)
-    #define POLYHOOK2_OS_WINDOWS
-    #define POLYHOOK2_ARCH_X86
+#define POLYHOOK2_OS_WINDOWS
+#define POLYHOOK2_ARCH_X86
 #elif defined(__linux__) || defined(linux)
-    #if defined(__x86_64__)
-        #define POLYHOOK2_OS_LINUX
-        #define POLYHOOK2_ARCH_X64
-    #else
-        #define POLYHOOK2_OS_LINUX
-        #define POLYHOOK2_ARCH_X86
-    #endif
+#if defined(__x86_64__)
+#define POLYHOOK2_OS_LINUX
+#define POLYHOOK2_ARCH_X64
+#else
+#define POLYHOOK2_OS_LINUX
+#define POLYHOOK2_ARCH_X86
+#endif
 #elif defined(__APPLE__)
-    #if defined(__x86_64__)
-        #define POLYHOOK2_OS_APPLE
-        #define POLYHOOK2_ARCH_X64
-    #else
-        #define POLYHOOK2_OS_APPLE
-        #define POLYHOOK2_ARCH_X86
-    #endif
+#if defined(__x86_64__)
+#define POLYHOOK2_OS_APPLE
+#define POLYHOOK2_ARCH_X64
+#else
+#define POLYHOOK2_OS_APPLE
+#define POLYHOOK2_ARCH_X86
+#endif
 #endif
 
 #if defined(_MSC_VER)
-    #define PLH_INLINE __forceinline
+#define PLH_INLINE __forceinline
 #elif defined(__GNUC__)
-    #define PLH_INLINE __attribute__((always_inline))
+#define PLH_INLINE __attribute__((always_inline))
 #else
-    #define PLH_INLINE inline
+#define PLH_INLINE inline
 #endif
 
 #include <iostream> //for debug printing
